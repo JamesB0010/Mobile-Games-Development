5d26a71415e9da6d37bef6533c1640f4dae17ea3
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-10-17 14:40:01 +0100

    Started cleaning

--- Assets/Scripts/CameraFOVChanger.cs
+++ Assets/Scripts/CameraFOVChanger.cs
@@ -2,20 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
+using UnityEditor;
 using UnityEngine;
 
 public class CameraFOVChanger : MonoBehaviour
 {
-    CinemachineVirtualCamera virtualCamera;
-    
-    private PlayerMovement playerMovement;
-
+    //Attributes
     public bool zoomCamera = false;
-
+    
+    [Header("Configurables")]
     [SerializeField] private float zoomRate;
 
     [SerializeField]
     private float minFov, maxFov;
+    
+    //dependencies resolved in the start function
+    CinemachineVirtualCamera virtualCamera;
+    
+    private PlayerMovement playerMovement;
     private void Start()
     {
         this.playerMovement = FindObjectOfType<PlayerMovement>();
@@ -26,13 +30,21 @@ public class CameraFOVChanger : MonoBehaviour
     {
         if (this.zoomCamera)
         {
-            this.virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(this.virtualCamera.m_Lens.FieldOfView, 25, Time.deltaTime * this.zoomRate);
+            this.ZoomCameraForShooting();
             return;
         }
-        
+
+        MapThrottleToFOV();
+    }
+
+    private void ZoomCameraForShooting()
+    {
+        this.virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(this.virtualCamera.m_Lens.FieldOfView, 25, Time.deltaTime * this.zoomRate);
+    }
+    private void MapThrottleToFOV()
+    {
         float throttleAmount = playerMovement.Throttle;
 
         this.virtualCamera.m_Lens.FieldOfView = ValueInRangeMapper.Map(throttleAmount, 0, 1, this.minFov, this.maxFov);
-
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,60 +7,40 @@ using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private UnityEvent AlertEnemyKilled = new UnityEvent();
-
-    [SerializeField] private UnityEvent AlertRoundFinished = new UnityEvent();
+    //Attributes
+    [Header("Enemy Spawning")]
+    [SerializeField] EnemySpawnManager enemySpawnManager;
     
-    [SerializeField]
-    private float enemiesToSpawn;
-
-    [SerializeField] private Transform spawnBoundaryPositiveZ,
-        spawnBoundaryNegativeZ,
-        spawnBoundaryPositiveX,
-        spawnBoundaryNegativeX,
-        spawnBoundaryPositiveY,
-        spawnBoundaryNegativeY;
-
-    [SerializeField] private Enemy enemyPrefab;
-
-
-    private short activeEnemies = 0;
+    
+    [Header("Enemy Management")]
+    ActiveEnemiesManager activeEnemiesManager;
+    
+    
+    [Header("Events")]
+    [SerializeField] private UnityEvent AlertRoundFinished = new UnityEvent();
 
 
     private void Start()
     {
-        this.SpawnEnemies();
+        SpawnEnemies();
+        SubscribeToEvents();
     }
-
     private void SpawnEnemies()
     {
-        this.activeEnemies = 0;
-        for (int i = 0; i < this.enemiesToSpawn; i++)
-        {
-            this.activeEnemies++;
-            float minX = this.spawnBoundaryNegativeX.position.x;
-            float maxX = this.spawnBoundaryPositiveX.position.x;
-
-            float minY = this.spawnBoundaryNegativeY.position.y;
-            float maxY = this.spawnBoundaryPositiveY.position.y;
-
-            float minZ = this.spawnBoundaryNegativeZ.position.z;
-            float maxZ = this.spawnBoundaryPositiveZ.position.z;
-            
-            Enemy enemy = Instantiate(enemyPrefab, new Vector3(Random.Range(minX, minY), Random.Range(minY, maxY), Random.Range(minZ, maxZ)), Random.rotation);
-            enemy.GameManager = this;
-        }
+        this.activeEnemiesManager = new ActiveEnemiesManager();
+        short enemySpawnedCount = enemySpawnManager.SpawnEnemies(this, this.activeEnemiesManager);
+        activeEnemiesManager.ActiveEnemyCount = enemySpawnedCount;
     }
 
-    public void EnemyKilled()
+    private void SubscribeToEvents()
     {
-        this.AlertEnemyKilled?.Invoke();
-        this.activeEnemies--;
-
-        if (this.activeEnemies == 0)
-        {
-            this.AlertRoundFinished?.Invoke();
-        }
+        this.activeEnemiesManager.AllEnemiesDead += this.OnAllEnemiesDead;
     }
+
+
+    private void OnAllEnemiesDead()
+    {
+        this.AlertRoundFinished?.Invoke();
+    }
+
 }
