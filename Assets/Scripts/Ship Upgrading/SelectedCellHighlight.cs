8fad160d68a22bd7e74229e24624977b63515570
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-11-04 12:08:03 +0000

    Make cell highlight also be masked like the upgrade cells

--- Assets/Scripts/Ship Upgrading/SelectedCellHighlight.cs
+++ Assets/Scripts/Ship Upgrading/SelectedCellHighlight.cs
@@ -8,6 +8,8 @@ public class SelectedCellHighlight : MonoBehaviour
     private UpgradeCell selectedCell;
     public UpgradeCell SelectedCell => this.selectedCell;
 
+    private bool stickToCell = false;
+
     private static SelectedCellHighlight instance = null;
 
     private void Awake()
@@ -18,14 +20,22 @@ public class SelectedCellHighlight : MonoBehaviour
             Destroy(this.gameObject);
     }
 
+    private void Update()
+    {
+        if (this.stickToCell)
+            transform.position = selectedCell.transform.position;
+    }
+
     public void SelectCell(UpgradeCell cell)
     {
-        transform.position = cell.transform.position;
         this.selectedCell = cell;
+        this.stickToCell = true;
+        transform.position = cell.transform.position;
     }
 
     public void MoveOffScreen()
     {
+        this.stickToCell = false;
         transform.position = new Vector3(-3000, -3000, 0);
     }
 }
