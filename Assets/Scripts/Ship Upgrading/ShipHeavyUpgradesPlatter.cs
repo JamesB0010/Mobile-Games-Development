8f3fceda16d8e085b172fce2f7016b7045140b1e
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-11-06 14:30:58 +0000

    Just discovered something which needs to be added
    In the item shop when you click on an item to purchase you should see some stats below the scroll rect update with info about that item so you can know if you want to make the purchase

--- Assets/Scripts/Ship Upgrading/ShipHeavyUpgradesPlatter.cs
+++ Assets/Scripts/Ship Upgrading/ShipHeavyUpgradesPlatter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(menuName = "Upgrade Platters/Heavy Gun Upgrade Platter")]
 public class ShipHeavyUpgradesPlatter : ShipUpgradesPlatterBase
 {
