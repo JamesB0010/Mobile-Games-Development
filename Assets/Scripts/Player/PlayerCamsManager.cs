50a93a659582bf6bb494276278928cc6aa63488e
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-11-20 21:44:40 +0000

    Fixed problem with third person boost camera

--- Assets/Scripts/Player/PlayerCamsManager.cs
+++ Assets/Scripts/Player/PlayerCamsManager.cs
@@ -1,39 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Cinemachine;
 using UnityEngine;
 
-public class PlayerCamsManager : MonoBehaviour
+public sealed class PlayerCamsManager : MonoBehaviour
 {
+    
+    
     [SerializeField] private GameObject firstPersonCamera;
-    
-        [SerializeField] private CinemachineVirtualCamera firstPersonBoostCam;
-    
-        [SerializeField] private CinemachineVirtualCamera[] firstPersonVirtualCameras;
-        
-    
-        [SerializeField] private GameObject thirdPersonCamera;
-    
-        [SerializeField] private CinemachineVirtualCamera thirdPersonBoostCam;
-    
-        [SerializeField] private CinemachineVirtualCamera[] thirdPersonVirtualCameras;
 
-        public void CameraViewChanged(bool firstPerson)
+    [SerializeField] private CinemachineVirtualCamera firstPersonBoostCam;
+
+    [SerializeField] private CinemachineVirtualCamera[] firstPersonVirtualCameras;
+
+
+    [SerializeField] private GameObject thirdPersonCamera;
+
+    [SerializeField] private CinemachineVirtualCamera thirdPersonBoostCam;
+
+    [SerializeField] private CinemachineVirtualCamera[] thirdPersonVirtualCameras;
+
+    public void CameraViewChanged(bool firstPerson)
+    {
+        firstPersonCamera.SetActive(firstPerson);
+        foreach (var cam in this.firstPersonVirtualCameras)
         {
-            firstPersonCamera.SetActive(firstPerson);
-            foreach (var cam in this.firstPersonVirtualCameras)
-            {
-                cam.gameObject.SetActive(firstPerson);
-            }
-
-
-            this.thirdPersonCamera.SetActive(!firstPerson);
-            foreach (var cam in this.thirdPersonVirtualCameras)
-            {
-                cam.gameObject.SetActive(!firstPerson);
-            }
-
-            firstPersonBoostCam.gameObject.SetActive(false);
-            thirdPersonBoostCam.gameObject.SetActive(false);
+            cam.gameObject.SetActive(firstPerson);
         }
+
+
+        this.thirdPersonCamera.SetActive(!firstPerson);
+        foreach (var cam in this.thirdPersonVirtualCameras)
+        {
+            cam.gameObject.SetActive(!firstPerson);
+        }
+
+        firstPersonBoostCam.gameObject.SetActive(false);
+        thirdPersonBoostCam.gameObject.SetActive(false);
+    }
 }
