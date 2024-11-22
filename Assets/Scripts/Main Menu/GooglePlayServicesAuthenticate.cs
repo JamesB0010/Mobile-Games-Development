47fef29c9059821b3348583c0b43e6e5ada2a607
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-11-22 22:44:47 +0000

    added privacy policy and started trying to use authentication

--- Assets/Scripts/Main Menu/GooglePlayServicesAuthenticate.cs
+++ Assets/Scripts/Main Menu/GooglePlayServicesAuthenticate.cs
@@ -9,19 +9,36 @@ public class GooglePlayServicesAuthenticate : MonoBehaviour
 {
     private void Start()
     {
-        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        PlayGamesPlatform.Instance.Authenticate(this.ProcessAutoAuthentication);
     }
 
-    private void ProcessAuthentication(SignInStatus status)
+    private void ProcessAutoAuthentication(SignInStatus status)
     {
         if (status == SignInStatus.Success)
         {
-            //continue with play games services
-            Debug.Log("Sign in Successful");
+            this.OnSuccessfulSignIn();
         }
         else
         {
             Debug.Log("auto sign in didnt work maybe try manually auth");
+            PlayGamesPlatform.Instance.ManuallyAuthenticate(this.ProcessAutoAuthentication);
         }
+    }
+
+    private void ProcessManualAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            this.OnSuccessfulSignIn();
+        }
+        else
+        {
+            Debug.Log("Tried manual and it didnt work... cooked");
+        }
+    }
+
+    private void OnSuccessfulSignIn()
+    {
+            Debug.Log("Sign in Successful");
     }
 }
