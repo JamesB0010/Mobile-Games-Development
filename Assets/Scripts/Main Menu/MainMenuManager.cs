b39d2ef1378a32ca188f48e0fba8d138f3f4dcfb
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-12-04 21:41:04 +0000

    rough but some progress with setting up google play games authentication

--- Assets/Scripts/Main Menu/MainMenuManager.cs
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -1,15 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private PlayableDirector startGameDirector, part1IntroDirector;
+
+    [SerializeField] private UnityEvent SignInSucessEvent = new UnityEvent();
+
+    [SerializeField] private UnityEvent SignInFailedEvent = new UnityEvent();
+    
+    
+
+    private void Start()
+    {
+        PlayGamesPlatform.Instance.Authenticate(this.ProcessAuthentication);
+    }
+
+    private void ProcessAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            //signed in
+            this.SignInSucess();
+        }
+        else
+        {
+            PlayGamesPlatform.Instance.ManuallyAuthenticate(this.ProcessManualAuthentication);
+        }
+    }
+
+    private void SignInSucess()
+    {
+        this.SignInSucessEvent?.Invoke();
+    }
+
+    private void ProcessManualAuthentication(SignInStatus status)
+    {
+        if (status == SignInStatus.Success)
+        {
+            //yay
+            this.SignInSucess();
+        }
+        else
+        {
+            //cooked
+            this.SignInFailedEvent?.Invoke();
+        }
+    }
+
     public void EnterGameButtonPressed()
     {
         part1IntroDirector.Play();
+        
     }
     public void IntroFirstPartComplete()
     {
