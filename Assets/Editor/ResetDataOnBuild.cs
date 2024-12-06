2c569609e0f9649c0a307dd0ea8ea0872cd4bdf4
James Bland <98528984+JamesB0010@users.noreply.github.com>
2024-12-06 18:56:06 +0000

    Added wat sould reset the scriptable objects on build

---
+++ Assets/Editor/ResetDataOnBuild.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class ResetDataOnBuild : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        ResetScriptableObjects.ResetObjects();
+    }
+}
