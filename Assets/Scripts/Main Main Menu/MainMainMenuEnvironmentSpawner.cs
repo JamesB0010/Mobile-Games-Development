57eee40bc2ae9414c7b7d297a39521da368b70a0
James Bland <98528984+JamesB0010@users.noreply.github.com>
2025-01-12 21:42:14 +0000

    new main menu done

--- Assets/Scripts/Main Main Menu/MainMainMenuEnvironmentSpawner.cs
+++ Assets/Scripts/Main Main Menu/MainMainMenuEnvironmentSpawner.cs
@@ -6,10 +6,10 @@ using UnityEngine.AddressableAssets;
 
 public class MainMainMenuEnvironmentSpawner : MonoBehaviour
 {
-    [SerializeField] private AssetReference asterioids;
+    [SerializeField] private AssetReference mainMainMenuAsteriods;
 
-    [SerializeField] private Transform[] asteriodPlaces;
-
+    [SerializeField] private Transform spawnPos;
+    
     [SerializeField] private AssetReference buzzardShip;
 
     [SerializeField] private Transform buzzardShipSpawn;
@@ -18,14 +18,15 @@ public class MainMainMenuEnvironmentSpawner : MonoBehaviour
 
     [SerializeField] private Transform[] harbSpawns;
 
+
     private void Start()
     {
-        SpawnThings(asterioids, asteriodPlaces);
+        SpawnThings(this.mainMainMenuAsteriods, new[]{spawnPos});
         SpawnThings(buzzardShip, new Transform[]{buzzardShipSpawn});
-        SpawnThings(harb, harbSpawns);
+        SpawnThings(harb, harbSpawns, new Vector3(-90, 0,0));
     }
 
-    private void SpawnThings(AssetReference thingToSpawn, Transform[] spawnTransforms)
+    private void SpawnThings(AssetReference thingToSpawn, Transform[] spawnTransforms, Vector3 rotationToAdd = new Vector3())
     {
         for (int i = 0; i < spawnTransforms.Length; i++)
         {
@@ -35,7 +36,11 @@ public class MainMainMenuEnvironmentSpawner : MonoBehaviour
                 Transform trans = handle.Result.transform;
                 trans.position = spawnTransforms[j].position;
                 trans.rotation = spawnTransforms[j].rotation;
+                trans.localRotation = trans.localRotation * Quaternion.Euler(rotationToAdd);
                 trans.localScale = spawnTransforms[j].localScale;
+
+                if (trans.GetChild(0).TryGetComponent(out LODGroup group))
+                    group.enabled = false;
             };
         }
     }
